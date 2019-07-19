9801eb2de562a04ec7e88213726744ef4a4a3d4b
Mert Selimbeyoğlu <mertselimb@gmail.com>
2019-07-19 23:11:23 +0300

    minor name change

--- detect.py
+++ detect.py
@@ -1,77 +1,77 @@
 import cv2
 import numpy as np
 import dlib
-from math import hypot
+from math import hypot,degrees,atan2
 
 
 def generateNewGlass(frame):
     landmarks = predictor(gray_frame, face)
 
-    # Nose coordinates
-    top_nose = (landmarks.part(23).x, landmarks.part(23).y)
-    center_nose = (landmarks.part(27).x, landmarks.part(27).y)
-    left_nose = (landmarks.part(0).x, landmarks.part(0).y)
-    right_nose = (landmarks.part(16).x, landmarks.part(16).y)
-    bottom_nose = (landmarks.part(30).x, landmarks.part(30).y)
+    top_glass = (landmarks.part(23).x, landmarks.part(23).y)
+    center_glass = (landmarks.part(27).x, landmarks.part(27).y)
+    left_glass = (landmarks.part(0).x, landmarks.part(0).y)
+    right_glass = (landmarks.part(16).x, landmarks.part(16).y)
+    bottom_glass = (landmarks.part(30).x, landmarks.part(30).y)
 
-    nose_width = int(hypot(left_nose[0] - right_nose[0],
-                           left_nose[1] - right_nose[1])*1.1)
-    nose_height = int(nose_width * 0.65)
+    radian = atan2(bottom_glass[1]-center_glass[1],bottom_glass[0]-center_glass[0])
+    print(degrees(radian)-90)
 
-    # New nose position
-    top_left = (int(center_nose[0] - nose_width / 2),
-                top_nose[1]-20)
-    bottom_right = (int(center_nose[0] + nose_width / 2),
-                    int(center_nose[1] + nose_height / 2))
+    glass_width = int(hypot(left_glass[0] - right_glass[0],
+                           left_glass[1] - right_glass[1])*1.1)
+    glass_height = int(glass_width * 0.65)
 
-    return nose_width, nose_height, top_left
+    top_left = (int(center_glass[0] - glass_width / 2),
+                top_glass[1]-20)
+    bottom_right = (int(center_glass[0] + glass_width / 2),
+                    int(center_glass[1] + glass_height / 2))
+
+    return glass_width, glass_height, top_left
 
 
-def generateImage(frame, nose_width, nose_height, top_left):
-    nose_pig = cv2.resize(nose_image, (nose_width, nose_height))
-    nose_pig_gray = cv2.cvtColor(nose_pig, cv2.COLOR_BGR2GRAY)
-    _, nose_mask = cv2.threshold(
-        nose_pig_gray, 25, 255, cv2.THRESH_BINARY_INV)
+def generateImage(frame, glass_width, glass_height, top_left):
+    resized_glass = cv2.resize(glass_image, (glass_width, glass_height))
+    resized_glass_gray = cv2.cvtColor(resized_glass, cv2.COLOR_BGR2GRAY)
+    _, glass_mask = cv2.threshold(
+        resized_glass_gray, 25, 255, cv2.THRESH_BINARY_INV)
 
-    nose_area = frame[top_left[1]: top_left[1] + nose_height,
-                      top_left[0]: top_left[0] + nose_width]
-    nose_area_no_nose = cv2.bitwise_and(
-        nose_area, nose_area, mask=nose_mask)
-    final_nose = cv2.add(nose_area_no_nose, nose_pig)
-    frame[top_left[1]: top_left[1] + nose_height,
-          top_left[0]: top_left[0] + nose_width] = final_nose
+    glass_area = frame[top_left[1]: top_left[1] + glass_height,
+                      top_left[0]: top_left[0] + glass_width]
+    glass_area_no_glass = cv2.bitwise_and(
+        glass_area, glass_area, mask=glass_mask)
+    final_glass = cv2.add(glass_area_no_glass, resized_glass)
+    frame[top_left[1]: top_left[1] + glass_height,
+          top_left[0]: top_left[0] + glass_width] = final_glass
     return frame
 
 
-# Loading Camera and Nose image and Creating mask
-cap = cv2.VideoCapture(1)
-nose_image = cv2.imread("images/glass2.png")
+cap = cv2.VideoCapture(0)
+glass_image = cv2.imread("images/glass2.png")
 
 
 _, frame = cap.read()
 rows, cols, _ = frame.shape
-nose_mask = np.zeros((rows, cols), np.uint8)
+glass_mask = np.zeros((rows, cols), np.uint8)
 
 # Loading Face detector
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 work = True
 
-nose_width, nose_height, top_left = None, None, None
+glass_width, glass_height, top_left = None, None, None
 while True:
     _, frame = cap.read()
-    nose_mask.fill(0)
+    glass_mask.fill(0)
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     faces = detector(frame)
 
     for face in faces:
         if(work):
-            nose_width, nose_height, top_left = generateNewGlass(frame)
-            frame = generateImage(frame, nose_width, nose_height, top_left)
+            glass_width, glass_height, top_left = generateNewGlass(frame)
+            frame = generateImage(frame, glass_width, glass_height, top_left)
             work = False
         else:
-            frame = generateImage(frame, nose_width, nose_height, top_left)
+            frame = generateImage(frame, glass_width, glass_height, top_left)
             work = True
 
     cv2.imshow("Frame", frame)
