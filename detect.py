6848a85dccc1d460ff777166503914531314fa68
Mert Selimbeyoğlu <mertselimb@gmail.com>
2019-07-19 17:25:45 +0300

    new version

--- detect.py
+++ detect.py
@@ -3,63 +3,73 @@ import numpy as np
 import dlib
 from math import hypot
 
+# Loading Camera and Nose image and Creating mask
 cap = cv2.VideoCapture(0)
+nose_image = cv2.imread("glass3.png")
 
+tmp = cv2.cvtColor(nose_image, cv2.COLOR_BGR2GRAY)
+_,alpha = cv2.threshold(tmp,0,255,cv2.THRESH_BINARY)
+b, g, r = cv2.split(nose_image)
+rgba = [b,g,r, alpha]
+nose_image = cv2.merge(rgba,4)
+
+_, frame = cap.read()
+rows, cols, _ = frame.shape
+nose_mask = np.zeros((rows, cols), np.uint8)
+
+# Loading Face detector
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
-glass_image = cv2.imread("glass1.png")
 
 while True:
     _, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    nose_mask.fill(0)
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = detector(gray)
-    image_resized = glass_image
+    faces = detector(frame)
     for face in faces:
-        x1 = face.left()
-        y1 = face.top()
-        x2 = face.right()
-        y2 = face.bottom()
+        landmarks = predictor(gray_frame, face)
 
-        landmarks = predictor(gray, face)
+        # Nose coordinates
+        top_nose = (landmarks.part(23).x, landmarks.part(23).y)
+        center_nose = (landmarks.part(27).x, landmarks.part(27).y)
+        left_nose = (landmarks.part(0).x, landmarks.part(0).y)
+        right_nose = (landmarks.part(16).x, landmarks.part(16).y)
+        bottom_nose = (landmarks.part(30).x, landmarks.part(30).y)
 
-        top_area = (landmarks.part(43).x, landmarks.part(43).y)
-        center_area = (landmarks.part(27).x, landmarks.part(27).y)
-        left_area = (landmarks.part(0).x, landmarks.part(0).y)
-        right_area = (landmarks.part(16).x, landmarks.part(16).y)
-        bottom_area = (landmarks.part(30).x, landmarks.part(30).y)
+        nose_width = int(hypot(left_nose[0] - right_nose[0],
+                           left_nose[1] - right_nose[1])*1.1)
+        nose_height = int(nose_width * 0.65)
 
-        area_width = int(
-            hypot(left_area[0] - right_area[0], left_area[1] - right_area[1]))
-        area_height = int(
-            hypot(top_area[0] - bottom_area[0], top_area[1] - bottom_area[1]))
+        # New nose position
+        top_left = (int(center_nose[0] - nose_width / 2),
+                              top_nose[1]-16)
+        bottom_right = (int(center_nose[0] + nose_width / 2),
+                       int(center_nose[1] + nose_height / 2))
 
-        top_left = (int(center_area[0] - area_width / 2),
-                    int(center_area[1] - area_height / 2))
-        bottom_right = (int(center_area[0] + area_width / 2),
-                        int(center_area[1] + area_height / 2))
 
-        cv2.rectangle(frame, top_left, bottom_right, (0, 255, 0), 2)
+        # Adding the new nose
+        nose_pig = cv2.resize(nose_image, (nose_width, nose_height))
+        nose_pig_gray = cv2.cvtColor(nose_pig, cv2.COLOR_BGR2GRAY)
+        _, nose_mask = cv2.threshold(nose_pig_gray, 25, 255, cv2.THRESH_BINARY_INV)
 
-        image_resized = cv2.resize(glass_image, (area_width, area_height))
+        nose_area = frame[top_left[1]: top_left[1] + nose_height,
+                    top_left[0]: top_left[0] + nose_width]
+        nose_area_no_nose = cv2.bitwise_and(nose_area, nose_area, mask=nose_mask)
+        final_nose = cv2.add(nose_area_no_nose, nose_pig)
 
-        image_resized_gray = image_resized
-        _, glass_image = cv2.threshold(
-            image_resized_gray, 25, 255, cv2.THRESH_BINARY_INV)
+        frame[top_left[1]: top_left[1] + nose_height,
+                    top_left[0]: top_left[0] + nose_width] = final_nose
 
-        glass_area = frame[top_left[1]: top_left[1] + area_height,
-                           top_left[0]: top_left[0] + area_width]
+        # cv2.imshow("Nose area", nose_area)
+        # cv2.imshow("Nose pig", nose_pig)
+        # cv2.imshow("final nose", final_nose)
 
-        glass_area_inv = cv2.bitwise_and(
-            glass_area, glass_area, mask=glass_image)
 
-        final_image = cv2.add(glass_area_inv, image_resized)
-
-        frame[top_left[1]: top_left[1] + area_height,
-              top_left[0]: top_left[0] + area_width] = final_image
 
     cv2.imshow("Frame", frame)
-    # cv2.imshow("Resized", image_resized)
+
+
 
     key = cv2.waitKey(1)
     if key == 27:
